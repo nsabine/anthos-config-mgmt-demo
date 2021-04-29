39a60803b7425fa623d8827c02f58e6d963b7c77
Nick Sabine <nsabine@google.com>
2021-04-29 16:00:49 -0400

    fix reset script

--- demo-script/reset-branches.sh
+++ demo-script/reset-branches.sh
@@ -1,4 +1,11 @@
 git checkout main
+git pull
 git branch -d new-app
 git push origin --delete new-app
+
+git rm ../policy/namespaces/dlp/*
+git rm ../policy/namespaces/dlp
+
+git commit -m "removing dlp deployment"
+git push
 
