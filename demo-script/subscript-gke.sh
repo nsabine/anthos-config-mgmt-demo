6cd953b119a497dacad8c62edfa1857eff0386f6
Nick Sabine <nsabine@google.com>
2021-05-13 09:13:28 -0400

    show create commands

--- demo-script/subscript-gke.sh
+++ demo-script/subscript-gke.sh
@@ -45,6 +45,9 @@ gcloud container clusters list | grep ${CLUSTER_NAME} > /dev/null 2>&1
 if [ "$?" -gt "0" ]; then
   pei "gcloud container clusters create ${CLUSTER_NAME} --zone=${ZONE} --workload-pool=${PROJECT_ID}.svc.id.goog --enable-stackdriver-kubernetes"
 else
+  TYPE_SPEED=100
+  p "gcloud container clusters create ${CLUSTER_NAME} --zone=${ZONE} --workload-pool=${PROJECT_ID}.svc.id.goog --enable-stackdriver-kubernetes"
+  TYPE_SPEED=$DEMO_SPEED
   pei "# ... the Kubernetes Cluster is already running!"
 fi 
 
@@ -76,6 +79,9 @@ gcloud beta container hub memberships describe ${CLUSTER_NAME} > /dev/null 2>&1
 if [ "$?" -gt "0" ]; then
   pei "gcloud beta container hub memberships register ${CLUSTER_NAME} --gke-cluster=${REGION}/${CLUSTER_NAME} --enable-workload-identity"
 else
+  TYPE_SPEED=100
+  p "gcloud beta container hub memberships register ${CLUSTER_NAME} --gke-cluster=${REGION}/${CLUSTER_NAME} --enable-workload-identity"
+  TYPE_SPEED=$DEMO_SPEED
   pei "# ... Anthos Config Management membership already exists, moving on..."
 fi
 
@@ -84,6 +90,9 @@ gcloud beta container hub memberships describe ${ONPREM_CLUSTER_NAME} > /dev/nul
 if [ "$?" -gt "0" ]; then
   pei "gcloud beta container hub memberships register ${ONPREM_CLUSTER_NAME} --kubeconfig=${ONPREM_KUBECONFIG} --enable-workload-identity"
 else
+  TYPE_SPEED=100
+  p "gcloud beta container hub memberships register ${ONPREM_CLUSTER_NAME} --kubeconfig=${ONPREM_KUBECONFIG} --enable-workload-identity"
+  TYPE_SPEED=$DEMO_SPEED
   pei "# ... Anthos Config Management membership already exists, moving on..."
 fi
 
